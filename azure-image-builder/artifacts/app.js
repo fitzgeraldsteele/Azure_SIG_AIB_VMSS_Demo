0abfcaf0ca8376e11f2b1430be4c2f853cbf19d0
Jerry Steele <jerry.steele@microsoft.com>
2019-11-12 22:32:29 -0800

    Initial commit

---
+++ azure-image-builder/artifacts/app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+var os = require('os');
+
+app.get('/', function (req, res) {
+  const request = require('request');
+  const options = {
+    url: 'http://169.254.169.254/metadata/instance?api-version=2019-03-11',
+    headers: {
+      'Metadata': 'true'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var metadataObj = JSON.parse(body)
+      let ejs = require('ejs'),
+        html = ejs.render(
+          '<h2><%= metadataObj.compute.name %></h2> \
+            <p>Name: <%= metadataObj.compute.name %></p> \
+            <p>Private IP Address: <%= metadataObj.network.interface[0].ipv4.ipAddress[0].privateIpAddress %></p> \
+            <p>Resource id: <%= metadataObj.compute.resourceId %></p> \
+            <p>Scale set name: <%= metadataObj.compute.vmScaleSetName %></p></p> \
+            <p><pre><%=  JSON.stringify(metadataObj, null, 2) %></pre></p>',
+            { metadataObj: metadataObj }
+        )
+      res.send(html)
+    }
+    else {
+      console.log("Error " + response.statusCode)
+    }
+  })
+})
+app.get('/health', function (req, res) {
+  res.send('PONG!')
+})
+app.listen(3000, function () {
+  console.log('Hello world app listening on port 3000!')
+})
